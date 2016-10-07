624e936974161f603d42909cf706f4047f8f75e3
slindquist3 <slindquist3@gmail.com>
2016-10-07 11:54:57 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,54 @@
 class Student
 
+  sql = SQLite3::Database.new('db/students.db')
+
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def save
+   sql = <<-SQL
+     INSERT INTO students (name, grade, id)
+     VALUES (?, ?, ?)
+   SQL
+
+   DB[:conn].execute(sql, self.name, self.grade)
+   @id = DB[:conn].execute("SELECT id FROM students WHERE id = (SELECT MAX(id) FROM students)").flatten!.join("").to_i
+
+ end
+
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+
+    sql = <<-SQL
+    DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
+
+  end
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
 end
